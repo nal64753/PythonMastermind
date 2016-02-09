fc7027701d020e83dda52203c1da5cc1d94e812b
Nathan Li <nal64753@bethel.edu>
2016-02-09 15:48:16 -0600

    yet another logic rework using 'X' placeholders

--- mastermind.py
+++ mastermind.py
@@ -12,7 +12,7 @@ class GuessNum(object):
 # value: randomly generated digits put together into a string
 class Answer(object):
     guesses = 0
-
+    cowsnbulls = ['X', 'X', 'X', 'X']
     # instantiates the value of the answer
     def __init__(self):
         self.value = list(self.generate_number())
@@ -43,33 +43,50 @@ def user_guess():
 
 # compares user's guess to the answer and reports cows and bulls
 # returns the number of cows and bulls in an array
-def evaluate_guess(answer, guess):
-    cowbull = [0, 0]
+def evaluate_guess(answer, guess, cowsnbulls):
+    cowbull = cowsnbulls
     # iterates through all digits in the answer
     for i in range(4):
-        if answer[i] == guess[i]:
-            cowbull[0] += 1
-            continue
         if guess[i] in answer:
-            cowbull[1] += 1
-            continue
-    return cowbull
+            # cow check
+            if answer[i] == guess[i]:
+                cowbull[i] = 'C'
+                answer[i] = 'X'
+                guess[i] = 'X'
+
+    for j in range(4):
+        if guess[j] in answer and cowbull[j] != 'C':
+            cowbull[j] = 'B'
+
+    numcows = 0
+    numbulls = 0
+    for x in range (4):
+        if cowbull[x] == 'C':
+            numcows += 1
+        elif cowbull[x] == 'B':
+            numbulls += 1
+
+    return [numcows, numbulls]
 
 # Initiating the game
 ans = Answer()
-ans.value = ['1', '1', '2', '2']
+solution = ans.value
+# ans.value = ['1', '1', '2', '2']
 print 'Welcome to the Cows and Bulls Game! \n'
 cows = 0
 bulls = 0
 # Playing the game
 while cows != 4:
+    print solution
     usr = GuessNum(user_guess())
-    result = evaluate_guess(ans.value, usr.guess)
+    result = evaluate_guess(ans.value, usr.guess, ans.cowsnbulls)
     cows = result[0]
     bulls = result[1]
     ans.guesses += 1
+    ans.value = solution
     print 'you have %d cows, and %d bulls.' %(cows, bulls)
     print '(key: %s guess: %s) \n' % (ans.value, usr.guess)
+
 
 # After the game is completed
 print 'You completed the game! It took you %d guesses!' % ans.guesses
